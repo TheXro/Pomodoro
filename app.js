d0358f3aeee7245fddd89e33171fa75b055381cb
TheXro <ravyyadav97@gmail.com>
2023-06-03 19:54:35 +0530

    added blur when modal opens  and close when clicked outside

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const settingsModal = document.querySelector(".settingsModal")
 const settings = document.querySelector('.settings');
 const close = document.querySelector('.X')
+const overlay = document.querySelector('.overlay');
 
 const openModal = function () {
     console.log('Button Clicked');
     settingsModal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+
 };
 
 const closeModal = function () {
     console.log('Button Clicked');
     settingsModal.classList.add('hidden');
+    overlay.classList.add('hidden');
 };
-
+overlay.addEventListener('click', closeModal);
 
 settings.addEventListener('click', openModal)
 
