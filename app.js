b6bf50eb8afd744ada6de7f3a0d2732cc26bd5ce
TheXro <ravyyadav97@gmail.com>
2023-06-10 20:15:57 +0530

    added more js

--- app.js
+++ app.js
@@ -22,3 +22,17 @@ settings.addEventListener('click', openModal);
 //timer function
 const time = document.querySelector('.time');
 const pause = document.querySelector('.pause');
+
+
+//getting the time and updating the value of time
+const pmdr = document.querySelector('.pmdr');
+const shtbrk = document.querySelector('.shrtbrk');
+const lngbrk = document.querySelector('.lngbrk');
+const applybtn = document.querySelector('.applybtn');
+applybtn.addEventListener('click', function () {
+    console.log('Button Clicked');
+    time.textContent = pmdr.value + ':' + '00';
+    closeModal();
+}
+)
+
